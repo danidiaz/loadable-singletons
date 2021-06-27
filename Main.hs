8d81c06f6ca8decf6f3e64982e230677f37447ad
Daniel Diaz <diaz.carrete@facebook.com>
2021-06-27 18:15:25 +0200

    parser

--- Main.hs
+++ Main.hs
@@ -10,15 +10,7 @@ data Command = Pack {baseFolder :: FilePath, exePath :: FilePath} deriving (Show
 
 parserInfo :: O.ParserInfo Command
 parserInfo =
-  let pack :: O.ParserInfo Command
-      pack =
-        makeParserInfo
-          ( Pack
-              <$> O.strArgument (O.metavar "PACKAGE_FOLDER" <> O.help "Base package folder on which to seek resources.")
-              <*> O.strArgument (O.metavar "EXECUTABLE_PATH" <> O.help "Path to the executable.")
-          )
-          (O.progDesc "Pack resource files into an sqlite database next to the executable.")
-      toplevel :: O.ParserInfo Command
+  let toplevel :: O.ParserInfo Command
       toplevel =
         makeParserInfo
           ( O.subparser
@@ -28,6 +20,14 @@ parserInfo =
               )
           )
           (O.progDesc "A resource file packer.")
+      pack :: O.ParserInfo Command
+      pack =
+        makeParserInfo
+          ( Pack
+              <$> O.strArgument (O.metavar "PACKAGE_FOLDER" <> O.help "Base package folder on which to seek resources.")
+              <*> O.strArgument (O.metavar "EXECUTABLE_PATH" <> O.help "Path to the executable.")
+          )
+          (O.progDesc "Pack resource files into an sqlite database next to the executable.")
       makeParserInfo p mods = O.info (O.helper <*> p) (O.fullDesc <> mods)
    in toplevel
 
